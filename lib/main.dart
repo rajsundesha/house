293914203956e4f23d40642d887ca0b047882405
rajsundesha <42098851+rajsundesha@users.noreply.github.com>
2024-12-06 18:44:01 +0530

    Update main.dart with improved structure and Riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:house_rental_app/providers/payment_provider.dart';
-import 'package:house_rental_app/providers/property_provider.dart';
-import 'package:house_rental_app/providers/tenant_provider.dart';
-import 'package:house_rental_app/providers/user_provider.dart';
-
-import 'package:house_rental_app/routes/app_routes.dart';
-import 'package:provider/provider.dart';
-import 'firebase_options.dart'; // Ensure this file exists in lib/
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'core/app.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => PropertyProvider()),
-        ChangeNotifierProvider(create: (_) => TenantProvider()),
-        ChangeNotifierProvider(create: (_) => PaymentProvider()),
-        ChangeNotifierProvider(create: (_) => UserProvider()),
-  
-      ],
-      child: MyApp(),
+    const ProviderScope(
+      child: RentalApp(),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'House Rental App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/',
-      onGenerateRoute: AppRoutes.generateRoute,
-    );
-  }
 }
