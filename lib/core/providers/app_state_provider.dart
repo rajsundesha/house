27544423a3e6d36330341945d6535f2a7737ad31
RAJ SUNDESHA <rajsundesha@RAJs-38.local>
2024-12-12 20:47:21 +0530

    Remove files from repo but keep locally

--- lib/core/providers/app_state_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-enum AppState {
-  initial,
-  authenticated,
-  unauthenticated
-}
-
-final appStateProvider = StateProvider<AppState>((ref) => AppState.initial);
